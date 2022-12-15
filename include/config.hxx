ddc211443750b5c74ae04d87cebd02badaeaee79
druzevda <druzev1@mail.ru>
2022-12-15 20:23:37 +0600

    add letters in config.hxx

---
+++ include/config.hxx
@@ -0,0 +1,9 @@
+#ifndef CONFIG_HXX_INCLUDED_______
+#define CONFIG_HXX_INCLUDED_______
+
+#include <string>
+
+constexpr uint32_t lettersAmount = 43;
+constexpr char letters[43]={" abcdefghijklmnopqrstuvwxyz!?\"$\'1234568790"};
+
+#endif // CONFIG_HXX_INCLUDED_______
