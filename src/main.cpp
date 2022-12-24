f3dc6c3788b3827f204571c815af87ccfaaa16b4
druzevda <druzev1@mail.ru>
2022-12-24 13:41:09 +0600

    very simple sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,25 @@
+#include "weighMaster.hxx"
+#include "findBetterText.hxx"
+#include <iostream>
+#include "typingSample.hxx"
+#include "scanTextToString.hxx"
 int main(){
+  for(int i = 0; i < lettersAmount; ++i){
+    lettersMap.insert({letters[i],i});
+  }
+  weighMaster personMaster(lettersAmount);
 
+  initscr();
+  getmaxyx(stdscr,Y_SIZE_WINDOW,X_SIZE_WINDOW);
+
+  personMaster = typingSample("English texts for beginners to practice reading and comprehension online and for free.");
+  while(true){
+    int betterText = findBetterText(personMaster.getWeights());
+    printw("your better text = %s", texts[betterText].c_str());
+    const std::string newText = scanTextToString(betterText);
+    personMaster = typingSample(newText);
+  }
+
+  endwin();
+  return 0;
 }
