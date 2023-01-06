0a79546fc96e9bca00cbb5e016339f15189c9b91
druzevda <druzev1@mail.ru>
2023-01-06 22:15:47 +0600

    refactor and logfile out

--- include/findBetterText.hxx
+++ include/findBetterText.hxx
@@ -6,9 +6,10 @@
 #include <fstream>
 #include "weighMaster.hxx"
 int findBetterText(const std::vector<double>& userWeighs, const std::vector<std::string>& texts){
+  std::fprintf(logfile,"in findBetterText\n");
   char buff[3000];
   weighMaster weights(lettersAmount);
-  int bestText = -1;
+  int bestText = 0;
   double bestValue = -1;
 
   for(int textCount = 0; textCount < texts.size(); ++textCount){
@@ -16,6 +17,7 @@ int findBetterText(const std::vector<double>& userWeighs, const std::vector<std:
     const std::string fullPathToText = textsFolder+currentTextName;
     std::ifstream file{fullPathToText};
     if(file.bad()){
+      std::fprintf(logfile,"fileNotOpen ->%s<- \n",fullPathToText.c_str());
       throw std::runtime_error{"uncorrect filename"};
     }
     while(file.getline(buff,3000)){
@@ -40,16 +42,18 @@ int findBetterText(const std::vector<double>& userWeighs, const std::vector<std:
     }
 
     weights.clear();
-
-    std::cout << weightForThisText << std::endl;
+    std::fprintf(logfile,"->%s<- weigh = %lf\n",fullPathToText.c_str(),weightForThisText);
     if(weightForThisText > bestValue){
       bestText = textCount;
       bestValue = weightForThisText;
     }
   }
+  std::fprintf(logfile,"bettertext->%s<- weigh = %lf\n\n\n",texts[bestText].c_str(),bestValue);
+  fflush(logfile);
   return bestText;
 }
 std::string constructBetterWords(const std::vector<double>& userWeighs, const std::vector<std::string>& words){
+  std::fprintf(logfile,"in constructBetterWords\n");
   weighMaster weights(lettersAmount);
   std::multimap<double,int,std::greater<double>> wordsMap;
   for(uint32_t wordsCount = 0; wordsCount < words.size(); ++wordsCount){ const std::string& curWord = words[wordsCount];
@@ -77,17 +81,21 @@ std::string constructBetterWords(const std::vector<double>& userWeighs, const st
   std::string result{};
   for(const auto& [weigh,wordNum] : wordsMap){
     if(result.size() < maxTextFromWordsSize){
-      result += words[wordNum];
+      const std::string& goodWord=words[wordNum];
+      result += goodWord;
+      std::fprintf(logfile,"word->%s weigh->%lf\n",goodWord.c_str(),weigh);
       if(result.size() < maxTextFromWordsSize)
         result += " ";
     }else{
       break;
     }
   }
+  fflush(logfile);
   return result;
 }
 
 std::string constructTextFromWord(const std::string& word){
+  std::fprintf(logfile,"in constructTextFromWord word=%s\n",word.c_str());
   std::string result{};
   while(result.size() < maxTextFromWordsSize){
     result += word;
@@ -97,10 +105,12 @@ std::string constructTextFromWord(const std::string& word){
       result+= " ";
     }
   }
+  fflush(logfile);
   return result;
 }
 
 std::string findBetterWord(const std::vector<double>& userWeighs, const std::vector<std::string>& words){
+  std::fprintf(logfile,"in findBetterWord\n");
   weighMaster weights(lettersAmount);
   std::multimap<double,int,std::greater<double>> wordsMap;
   for(uint32_t wordsCount = 0; wordsCount < words.size(); ++wordsCount){ const std::string& curWord = words[wordsCount];
@@ -125,7 +135,13 @@ std::string findBetterWord(const std::vector<double>& userWeighs, const std::vec
     weights.clear();
     wordsMap.insert({weightForThisText,wordsCount});
   }
-  const auto bestWord = (*wordsMap.begin()).second;
-  return words[bestWord];
+  const auto bestWordPlace = (*wordsMap.begin()).second;
+  const auto bestWordWeigh = (*wordsMap.begin()).first;
+  const std::string& bestWord = words[bestWordPlace];
+
+  std::fprintf(logfile,"best word =%s weigh=%lf\n",bestWord.c_str(),bestWordWeigh);
+
+  fflush(logfile);
+  return bestWord;
 }
 #endif  // FIND_BETTER_TEXT_HXX_INCLUDED
