fc32611f9f465fad3c6f3a8b164d1074d419f87f
druzevda <druzev1@mail.ru>
2023-01-12 00:55:23 +0600

    start count time from first symbol

--- include/typingSample.hxx
+++ include/typingSample.hxx
@@ -65,7 +65,7 @@ weighMaster typingSample(const std::string& text){
 
   const int lettersInText = text.size();
 
-  const auto begin = std::chrono::system_clock::now();
+  auto begin = std::chrono::system_clock::now();
 
   auto printStat=[&begin](WINDOW* curWin,const std::string& header,const int errorsAmount,const int goodSymbols){
     wclear(curWin);
@@ -125,6 +125,8 @@ weighMaster typingSample(const std::string& text){
     printText();
 
     const char c = getch();
+    if(allSum==0)
+      begin = std::chrono::system_clock::now();
 
     if(allSum > 0 && c == 127){ // delit
       if(allSum == goodSym)
