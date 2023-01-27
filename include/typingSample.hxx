1c5ad11fa84f2866d53dd4ef81c276ed5c54fe6f
druzevda <druzev1@mail.ru>
2023-01-27 16:16:52 +0600

    return exit code

--- include/typingSample.hxx
+++ include/typingSample.hxx
@@ -3,7 +3,8 @@
 
 #include "weighMaster.hxx"
 #include <string>
+#include "enumCodes.hxx"
 
-weighMaster typingSample(const std::string& text);
+weighMaster typingSample(const std::string& text, EXITCODE_TS& exitcode);
 
 #endif // TYPING_SAMPLE_HXX_INCLUDED___
