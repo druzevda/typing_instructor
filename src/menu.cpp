a95cb8806fb1e04b2fe6c6db0d9eb3214208f168
druzevda <druzev1@mail.ru>
2023-02-06 15:59:18 +0600

    menu shortcat and refactor

--- src/menu.cpp
+++ src/menu.cpp
@@ -6,7 +6,6 @@
 #include <vector>
 #include "config.hxx"
 
-
 void learningByErrorsTextes_mode();
 void randomText_mode();
 void learningByErrorsWords_mode();
@@ -16,9 +15,6 @@ void randomWord_mode();
 void randomLetters_mode();
 
 void menu(){
-  int c;
-  int curItemNum=0;
-  MENU *my_menu;
 
   assert(menu_choices.size()==menu_descriptions.size());
 
@@ -37,22 +33,38 @@ void menu(){
   set_item_userptr(my_items[5],(void*)randomWord_mode);
   set_item_userptr(my_items[6],(void*)randomLetters_mode);
 
-  my_menu = new_menu((ITEM **)my_items);
+  MENU *my_menu = new_menu((ITEM **)my_items);
   post_menu(my_menu);
+
+  const uint32_t minMenuItemNum = 0;
+  const uint32_t maxMenuItemNum = menu_descriptions.size();
+
+  int c;
+  int curItemNum=0;
 
   while((c = getch()) != KEY_F(1))
   {
+    const int64_t realNumKey = c-'0';
+    if(realNumKey >= minMenuItemNum && realNumKey < maxMenuItemNum){
+      void (*mode)() = (void(*)())item_userptr(my_items[realNumKey]);
+      clear();
+      mode();
+      menu_driver(my_menu, REQ_UP_ITEM);
+      menu_driver(my_menu, REQ_DOWN_ITEM);
+    }
     switch(c) {
       case KEY_DOWN:
       case 'j':
         --curItemNum;
         menu_driver(my_menu, REQ_DOWN_ITEM);
       break;
+
       case 'k':
       case KEY_UP:
         ++curItemNum;
         menu_driver(my_menu, REQ_UP_ITEM);
       break;
+
       case 10:
         ITEM * cur_item = current_item(my_menu);
         void (*mode)() = (void(*)())item_userptr(cur_item);
@@ -71,5 +83,6 @@ void menu(){
   free_item(my_items[4]);
   free_item(my_items[5]);
   free_item(my_items[6]);
+  unpost_menu(my_menu);
   free_menu(my_menu);
 }
